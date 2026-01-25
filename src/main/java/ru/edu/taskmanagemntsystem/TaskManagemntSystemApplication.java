68cbf87a280fc40d0db24e975131c22e437ccf63
att291K <att291@mail.ru>
2026-01-25 14:04:00 +0300

    тест

--- src/main/java/ru/edu/taskmanagemntsystem/TaskManagemntSystemApplication.java
+++ src/main/java/ru/edu/taskmanagemntsystem/TaskManagemntSystemApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TaskManagemntSystemApplication {
 
-
     public static void main(String[] args) {
         SpringApplication.run(TaskManagemntSystemApplication.class, args);
     }
